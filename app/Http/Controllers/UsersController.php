de3634286a0e4396aa377a3e29f1be16d175d234
hmonline <37518585@qq.com>
2018-08-27 08:13:55 +0000

    add admin

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,16 +10,39 @@ use Auth;
 
 class UsersController extends Controller
 {
+    //仅以下几个 指定动作 不使用 Auth 中间件进行过滤，意为 —— 除了此处指定的动作以外，所有其他动作都必须登录用户才能访问
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index']
+        ]);
+
+        //只让未登录用户访问注册页面：
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+    //用户列表首页
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
+    //用户注册页面
     public function create()
     {
         return view('users.create');
     }
 
+    //用户个人资料
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
+    //用户注册保存
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -37,5 +60,34 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
+    }
+
+    //用户信息修改
+    public function edit(User $user)
+    {
+         $this->authorize('update', $user);
+         return view('users.edit', compact('user'));
+    }
+
+    //用户信息修改保存
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
     }
 }
