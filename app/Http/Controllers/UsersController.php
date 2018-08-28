eac7bf582650970221098328f6ea2afa0b80cfe1
hmonline <37518585@qq.com>
2018-08-28 08:24:27 +0000

    Add user following

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -133,4 +133,20 @@ class UsersController extends Controller
         session()->flash('success', '成功删除用户！');
         return back();
     }
+
+    //显示用户关注人列表
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    //显示粉丝列表
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = '粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Notifications\ResetPassword;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -54,10 +55,49 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
-    //该方法将当前用户发布过的所有微博从数据库中取出，并根据创建时间来倒序排序
+    //该方法将当前用户关注用户的所有微博从数据库中取出，并根据创建时间来倒序排序
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = Auth::user()->followings->pluck('id')->toArray();
+        array_push($user_ids, Auth::user()->id);
+        return Status::whereIn('user_id', $user_ids)
+                              ->with('user')
+                              ->orderBy('created_at', 'desc');
+    }
+
+    //获取粉丝关系列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    //获取用户关注人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    //关注
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    //取消关注
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    //查看用户是否关注
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
     }
 }
